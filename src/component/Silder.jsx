436b4b38e717cfe72f72b2b339c629e7cce58d35
unknown <sizwenqwala@gmail.com>
2023-05-15 09:13:19 +0200

    added update mt-5

--- src/component/Silder.jsx
+++ src/component/Silder.jsx
@@ -4,29 +4,33 @@ import Carousel from 'react-bootstrap/Carousel';
 const Silder = () => {
   return (
     <div>
-    <Carousel fade>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://amazonproone.vercel.app/static/media/img2.bc1bdb910ead16c65197.jpg"
-          alt="First slide"
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://amazonproone.vercel.app/static/media/img5.aa945e25375bfdee385f.jpg"
-          alt="Second slide"
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://amazonproone.vercel.app/static/media/img3.c80809bb40bee5c34372.jpg"
-          alt="Third slide"
-        />
-      </Carousel.Item>
-    </Carousel>
+      <Carousel fade>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://amazonproone.vercel.app/static/media/img2.bc1bdb910ead16c65197.jpg"
+            alt="First slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://amazonproone.vercel.app/static/media/img5.aa945e25375bfdee385f.jpg"
+            alt="Second slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://amazonproone.vercel.app/static/media/img3.c80809bb40bee5c34372.jpg"
+            alt="Third slide"
+          />
+        </Carousel.Item>
+      </Carousel>
+
+      <div className='container mt-5'>
+        <p class="text-center display-9 my-1"><strong>Less time and energy wasted. <br></br>Keep reading through these shopping quotes from TV shows and<br></br> movies and people who love shopping to better understand why it makes them happy.</strong></p>
+      </div>
     </div>
   )
 }
