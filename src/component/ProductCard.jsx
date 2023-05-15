67dc41e6fbb6a4c5c585ada98986d66e0807bbb1
unknown <sizwenqwala@gmail.com>
2023-05-15 10:15:20 +0200

    added card center

--- src/component/ProductCard.jsx
+++ src/component/ProductCard.jsx
@@ -4,15 +4,42 @@ import Card from 'react-bootstrap/Card';
 
 const ProductCard = () => {
   return (
-    <div>
-       <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="https://images.pexels.com/photos/2584269/pexels-photo-2584269.jpeg?auto=compress&cs=tinysrgb&w=600" />
-      <Card.Body>
-        <div className='title'> Long sleeve Jacket</div>
-        <div className='price'>R2000 </div>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+    <div className='Container mt-5 d-flex justify-content-center'>
+      <Card style={{ width: '18rem' }}>
+        <Card.Img src="https://images.pexels.com/photos/2584269/pexels-photo-2584269.jpeg?auto=compress&cs=tinysrgb&w=600" />
+        <Card.Body>
+          <div className='title'> Long sleeve Jacket</div>
+          <div className='price'>R2000 </div>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+
+      <Card style={{ width: '18rem' }}>
+        <Card.Img src="https://images.pexels.com/photos/2584269/pexels-photo-2584269.jpeg?auto=compress&cs=tinysrgb&w=600" />
+        <Card.Body>
+          <div className='title'> Long sleeve Jacket</div>
+          <div className='price'>R2000 </div>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+
+      <Card style={{ width: '18rem' }}>
+        <Card.Img src="https://images.pexels.com/photos/2584269/pexels-photo-2584269.jpeg?auto=compress&cs=tinysrgb&w=600" />
+        <Card.Body>
+          <div className='title'> Long sleeve Jacket</div>
+          <div className='price'>R2000 </div>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+
+      <Card style={{ width: '18rem' }}>
+        <Card.Img src="https://images.pexels.com/photos/2584269/pexels-photo-2584269.jpeg?auto=compress&cs=tinysrgb&w=600" />
+        <Card.Body>
+          <div className='title'> Long sleeve Jacket</div>
+          <div className='price'>R2000 </div>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
     </div>
   )
 }
