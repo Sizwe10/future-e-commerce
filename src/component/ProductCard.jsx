a06e2c5093d234de87430c5dd3a369321f586457
unknown <sizwenqwala@gmail.com>
2023-05-19 14:04:54 +0200

    added btn btn-dark

--- src/component/ProductCard.jsx
+++ src/component/ProductCard.jsx
@@ -8,33 +8,33 @@ const ProductCard = () => {
       <Card style={{ width: '18rem' }}>
         <Card.Img src="https://images.pexels.com/photos/2584269/pexels-photo-2584269.jpeg?auto=compress&cs=tinysrgb&w=600" />
         <Card.Body>
-          <div className='title'> Long sleeve Jacket</div>
+          <div className='title'><strong>Long sleeve Jacket</strong> </div>
           <div className='text-end'>R2000 </div>
-          <Button variant="primary">Add to Cart</Button>
+          <Button variant="btn btn-dark">Add to Cart</Button>
         </Card.Body>
       </Card>
       <Card style={{ width: '18rem' }}>
         <Card.Img src="https://images.pexels.com/photos/2584269/pexels-photo-2584269.jpeg?auto=compress&cs=tinysrgb&w=600" />
         <Card.Body>
-          <div className='title'> Long sleeve Jacket</div>
+          <div className='title'> <strong>Long sleeve Jacket</strong></div>
           <div className='text-end'>R2000 </div>
-          <Button variant="primary">Add to Cart</Button>
+          <Button variant="btn btn-dark">Add to Cart</Button>
         </Card.Body>
       </Card>
       <Card style={{ width: '18rem' }}>
         <Card.Img src="https://images.pexels.com/photos/2584269/pexels-photo-2584269.jpeg?auto=compress&cs=tinysrgb&w=600" />
         <Card.Body>
-          <div className='title'> Long sleeve Jacket</div>
+          <div className='title'> <strong>Long sleeve Jacket</strong> </div>
           <div className='text-end'>R2000 </div>
-          <Button variant="primary">Add to Cart</Button>
+          <Button variant="btn btn-dark">Add to Cart</Button>
         </Card.Body>
       </Card>
       <Card style={{ width: '18rem' }}>
         <Card.Img src="https://images.pexels.com/photos/2584269/pexels-photo-2584269.jpeg?auto=compress&cs=tinysrgb&w=600" />
         <Card.Body>
-          <div className='title'> Long sleeve Jacket</div>
+          <div className='title'><strong>Long sleeve Jacket</strong></div>
           <div className='text-end'>R2000 </div>
-          <Button variant="primary">Add to Cart</Button>
+          <Button variant="btn btn-dark">Add to Cart</Button>
         </Card.Body>
       </Card>
       
