2e7cd82bf173e5a235fb662893f76638da7975a9
unknown <sizwenqwala@gmail.com>
2023-05-22 13:21:00 +0200

    added add woman

--- src/component/ProductCard.jsx
+++ src/component/ProductCard.jsx
@@ -9,7 +9,7 @@ const ProductCard = () => {
         <Card.Img src="https://images.pexels.com/photos/2584269/pexels-photo-2584269.jpeg?auto=compress&cs=tinysrgb&w=600" />
         <Card.Body>
           <div className='title'><strong>Long sleeve Jacket</strong> </div>
-          <div className='text-end'> <del className='del'>R1300</del><strong>R2000</strong></div>
+          <div className='text-end'><strong className='woman'>woman</strong> <del className='del'>R1300</del><strong>R2000</strong></div>
           <Button variant="btn btn-dark">Add to Cart</Button>
         </Card.Body>
       </Card>
