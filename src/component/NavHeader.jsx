69069cad4f09fc71232ad500060cb3ac6e8c64eb
unknown <sizwenqwala@gmail.com>
2023-05-23 10:08:28 +0200

    added change link NavHeader

--- src/component/NavHeader.jsx
+++ src/component/NavHeader.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
+import { Link } from "react-router-dom"
 const NavHeader = () => {
   return (
     <div>
@@ -12,16 +12,21 @@ const NavHeader = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link to="home"><strong>Home</strong></Nav.Link>
-              <Nav.Link to="aboutUs"><strong>About Us</strong></Nav.Link>
-              <Nav.Link to="contactUs"><strong>Contact Us</strong></Nav.Link>
-              <Nav.Link to="blog"><strong>Blog</strong></Nav.Link>
+              <Link to="/" className='nav-link m-2 menu-item'>
+                Home </Link>
+              <Link to="aboutUs/" className='nav-link m-2 menu-item'>
+                AboutUs </Link>
+              <Link to="/contactUs" className='nav-link m-2 menu-item'>
+                ContactUs </Link>
+              <Link to="/blog" className='nav-link m-2 menu-item'>
+                Blog </Link>
+
             </Nav>
           </Navbar.Collapse>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
-              <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-            </svg><strong> 0</strong>
-          
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+          </svg><strong> 0</strong>
+
         </Container>
       </Navbar>
     </div>
--- src/index.js
+++ src/index.js
@@ -3,12 +3,28 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {createBrowserRouter, RouterProvider,} from "react-router-dom";
+
 import HomePage from './pages/HomePage';
+import AboutUsPage from './pages/AboutUsPage';
+import BlogPage from './pages/BlogPage';
+import ContactPage from './pages/ContactPage';
 
 const router = createBrowserRouter([
   {
     path: "",
     element: <HomePage/>,
+  },
+  {
+    path: "aboutUs",
+    element: <AboutUsPage/>,
+  },
+  {
+    path: "contactUs",
+    element: <ContactPage/>,
+  },
+  {
+    path: "blog",
+    element: <BlogPage/>,
   }
 ])
 ReactDOM.createRoot(document.getElementById("root")).render(
