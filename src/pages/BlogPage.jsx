0c4926a6e2abbf2f2a518d89816b4b3b49c72e91
unknown <sizwenqwala@gmail.com>
2023-06-02 11:07:31 +0200

    added footer/ add card more

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import NavHeader from '../component/NavHeader'
 import Card from 'react-bootstrap/Card';
+import Footer from '../component/Footer'
 
 const BlogPage = () => {
   return (
@@ -21,10 +22,41 @@ const BlogPage = () => {
 
               <span className='line'>Phasellus tincidunt ultricies massa, sed tincidunt urna porta id. Nam dapibus ultricies lectus,Vestibulum vulputate</span>
             </div>
+          
+          </div>
+        </div>
+
+        <div className="container text-center my-3">
+          <div class="row">
+            <Card style={{ width: '18rem' }}>
+              <Card.Img src="https://images.pexels.com/photos/2584269/pexels-photo-2584269.jpeg?auto=compress&cs=tinysrgb&w=600" />
+            </Card>
+            <div className="col">
+              <h3>GET THE WIMBLEDON LOOK AT FASHION CLOTHES</h3>
+
+              <span className='line'>Phasellus tincidunt ultricies massa, sed tincidunt urna porta id. Nam dapibus ultricies lectus,Vestibulum vulputate</span>
+            </div>
+          
+          </div>
+        </div>
+
+        <div className="container text-center my-3">
+          <div class="row">
+            <Card style={{ width: '18rem' }}>
+              <Card.Img src="https://images.pexels.com/photos/2584269/pexels-photo-2584269.jpeg?auto=compress&cs=tinysrgb&w=600" />
+            </Card>
+            <div className="col">
+              <h3>GET THE WIMBLEDON LOOK AT FASHION CLOTHES</h3>
+
+              <span className='line'>Phasellus tincidunt ultricies massa, sed tincidunt urna porta id. Nam dapibus ultricies lectus,Vestibulum vulputate</span>
+            </div>
+          
           </div>
         </div>
 
       </div>
+
+      <Footer />
     </div>
   )
 }
